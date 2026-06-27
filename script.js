a33d9361396b417ba04bea5ec595baea74cab4c2
varunanjh <varunanjh@gmail.com>
2026-06-27 12:18:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const bgMusic = document.getElementById('bgMusic');
+document.getElementById('startBtn').addEventListener('click', () => {
+    bgMusic.play();
+    alert("Adventure starting!"); 
+});
