cb8ebf26f53ba15973d825e4de4f1336b3c4f2a3
varunanjh <varunanjh@gmail.com>
2026-06-27 12:39:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const bgMusic = document.getElementById('bgMusic');
+const gallery = document.getElementById('gallery');
+const imgDisplay = document.getElementById('imgDisplay');
+let count = 1;
+
 document.getElementById('startBtn').addEventListener('click', () => {
     bgMusic.play();
-    alert("Adventure starting!"); 
+    document.querySelector('h1').classList.add('hidden');
+    document.getElementById('startBtn').classList.add('hidden');
+    gallery.classList.remove('hidden');
 });
+
+function nextImg() {
+    count++;
+    if (count > 6) count = 1;
+    imgDisplay.src = "photo" + count + ".jpg";
+}
